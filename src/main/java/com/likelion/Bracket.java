7b88b0b37eac4f7d190af120d778d026b7ed0164
psi5059 <psi505968@gmail.com>
2022-10-20 09:32:08 +0900

    Stack 복습

---
+++ src/main/java/com/likelion/Bracket.java
@@ -0,0 +1,9 @@
+package com.likelion;
+
+public class Bracket {
+    public static void main(String[] args) {
+        Stack01 st = new Stack01();
+
+        String brackets = "";
+    }
+}
---
+++ src/main/java/com/likelion/Stack02.java
@@ -0,0 +1,32 @@
+package com.likelion;
+
+public class Stack02 {
+
+    private Integer[] arr;
+    private int top = 0;
+
+    public Stack02() {
+        this.arr = new Integer[10000];
+    }
+
+    public Stack02(int size) {
+        this.arr = new Integer[size];
+    }
+
+    public Integer[] getArr() {
+        return arr;
+    }
+
+    public void push(int value) {
+        //10을 넣으면 arr[0] = 10
+        this.arr[top] = value;
+        this.top++;
+
+    }
+
+    public int pop() {
+        int value = this.arr[this.top -1];  //this.top = 2
+        this.top--;
+        return value;
+    }
+}
