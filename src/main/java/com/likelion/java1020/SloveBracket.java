8b5a8cbbae0f868b49974314e7960d05dc509b61
psi5059 <psi505968@gmail.com>
2022-10-21 10:43:22 +0900

    stack으로 괄호 풀기

--- src/main/java/com/likelion/java1020/SloveBracket.java
+++ src/main/java/com/likelion/java1020/SloveBracket.java
@@ -1,11 +1,37 @@
 package com.likelion.java1020;
 
-public class SloveBracket {
-    public boolean solution(String s){
+import java.util.Arrays;
+import java.util.Stack;
 
+public class SloveBracket {
+    public boolean solution1(String s){
+        //stack을 쓰지 않고 풀 수는 있지만 효율성에서 시간초과.
         while(s.indexOf("()") >= 0){
             s = s.replace("()","");
         }
         return s.length() == 0;
+    }
+
+    public boolean solution2(String s){
+        //stack을 쓰지 않고 풀 수는 있지만 효율성에서 시간초과.
+        while (s.indexOf("()") >= 0){
+            String[] splitted = s.split("\\(\\)");
+            s = String.join("",splitted);
+        }
+        return s.length() == 0;
+    }
+
+    public boolean solution(String s){
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if('(' == s.charAt(i)){
+                st.push(s.charAt(i));
+            } else if (')' == s.charAt(i)) {
+                if(st.empty()) return false;
+                // 닫는 괄호부터 시작해서 비어있는 스택에 pop을 했을 때 처리
+                st.pop();
+            }
+        }
+        return st.empty();
     }
 }
--- src/test/java/com/likelion/java1020/SloveBracketTest.java
+++ src/test/java/com/likelion/java1020/SloveBracketTest.java
@@ -14,6 +14,8 @@ class SloveBracketTest {
         assertTrue(sb.solution("()()"));
         assertTrue(sb.solution("(())()"));
         assertFalse(sb.solution(")()("));
+        //닫는 괄호로 시작하기 때문에 스택이 pop부터 시도.
+        //하지만 스택이 비었기 때문에 pop을 할 수없어 EmptyStackException이 난다.
         assertFalse(sb.solution("(()("));
         assertFalse(sb.solution( "((((((((((((((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))))))))))))))))))))"));
 
