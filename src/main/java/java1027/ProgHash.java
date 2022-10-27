5db42f7b37114a3ebb2e82e443c50a7403f56b0d
psi5059 <psi505968@gmail.com>
2022-10-27 09:25:11 +0900

    프로그래머스 완주하지 못한 선수

---
+++ src/main/java/java1027/ProgHash.java
@@ -0,0 +1,9 @@
+package java1027;
+
+public class ProgHash {
+    // completion의 길이는 participant의 길이보다 1 작습니다.라는 말은
+    // participant 배열이 N이면 completion 배열은 N-1
+    // participant 배열에는 있지만 completion 배열에 없는 1명을 찾으면 될 것 같다.
+    // HashMap으로 participant 배열의 이름과 ?를  key와 value를 모두 입력해서
+    // completion에 동일하게 있는 사람을 1씩 빼면?
+}
