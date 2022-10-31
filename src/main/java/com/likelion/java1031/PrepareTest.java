2b7fcdb2822e38069c15da5e75c38dad45f368d7
psi5059 <psi505968@gmail.com>
2022-10-31 10:02:06 +0900

    프로그래머스 모의고사문제(점수 계산 추가) 진행중

--- src/main/java/com/likelion/java1031/PrepareTest.java
+++ src/main/java/com/likelion/java1031/PrepareTest.java
@@ -13,6 +13,7 @@ public class PrepareTest {
         int[] third = {3,3,1,1,2,2,4,4,5,5}; // 10개씩 반복
         int[] score = {0,0,0}; // 각 수포자들의 점수
 
+        //점수 계산
         for (int i = 0; i < answers.length; i++) {
             if (answers[i] == first[i%5]) score[0]++;
             if (answers[i] == first[i%8]) score[1]++;
