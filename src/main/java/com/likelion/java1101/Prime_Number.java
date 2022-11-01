ee26f0fd242a4eae87cef6529947f47792f0dee5
psi5059 <psi505968@gmail.com>
2022-11-01 09:35:19 +0900

    소수 구하기 알고리즘

---
+++ src/main/java/com/likelion/java1101/Prime_Number.java
@@ -0,0 +1,20 @@
+package com.likelion.java1101;
+
+public class Prime_Number {
+//1번 방법 : 13이라는 수가 있다면 2~12까지 나누어 본다. remainder가 0이 아니면 소수(%사용)
+    public boolean isPrime(int num){
+
+        for (int i = 2; i < num; i++) {
+            if ((num % i) == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Prime_Number pn = new Prime_Number();
+        System.out.println(pn.isPrime(13));
+    }
+
+}
