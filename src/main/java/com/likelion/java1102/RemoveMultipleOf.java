5e53844dbae9dded761c709bcd168294fcc6595b
psi5059 <psi505968@gmail.com>
2022-11-02 09:38:19 +0900

    에라토스테네스의 체 List 만들어서 2의 배수 지워보기

---
+++ src/main/java/com/likelion/java1102/RemoveMultipleOf.java
@@ -0,0 +1,36 @@
+package com.likelion.java1102;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveMultipleOf {
+    public int solution(int n) {
+        int answer = 0;
+
+        //List만들기
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 2; i < n; i++) {
+            list.add(i);
+        }
+        //2의 배수 지우기
+        for (int i = 0; i < list.size(); i++) {
+           if (list.get(i)%2 == 0){
+              list.remove(i);
+           }
+        }
+        //원소 출력해보기
+        for(int data : list){
+            System.out.println(data);
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        RemoveMultipleOf rmo = new RemoveMultipleOf();
+        rmo.solution(50);
+    }
+
+}
