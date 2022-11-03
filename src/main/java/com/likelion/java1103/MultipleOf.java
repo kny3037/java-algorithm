413768d40034d1bea6c5168a84e24e5166f0cf5d
psi5059 <psi505968@gmail.com>
2022-11-03 10:04:14 +0900

    에라토스테네스의 기본 세팅

--- src/main/java/com/likelion/java1103/MultipleOf.java
+++ src/main/java/com/likelion/java1103/MultipleOf.java
@@ -1,8 +1,11 @@
 package com.likelion.java1103;
 
+import java.util.Arrays;
+
 public class MultipleOf {
 
     public int solution(int n){
+        //배열은 0부터이니 원하는 수까지 구하려면 n+1개의 배열을 만들어줘야 한다.
         int[] prime = new int[n + 1];
         int answer = 0;
 
@@ -15,6 +18,13 @@ public class MultipleOf {
     }
 
     public static void main(String[] args) {
+        int N = 50;
+        int[] num = new int[N-1];  //N-1개 만큼 2~50까지. 총 49개
+        boolean[] checks = new boolean[num.length];  // num만큼 hchecks생성
+        Arrays.fill(checks, true); //check를 true로 초기화 -> 기본값 false
+        for (int i = 0; i < num.length; i++) { // 2~50까지
+            num[i] = i + 2;
 
+        }
     }
 }
