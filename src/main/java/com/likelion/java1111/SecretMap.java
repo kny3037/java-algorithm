5cf6534a8387ed101aeada1c274ff5e8daf5f464
psi5059 <psi5059@naver.com>
2022-11-11 10:26:29 +0900

    프로그래머스 비밀지도 개수에 맞는 이진수 배열

--- src/main/java/com/likelion/java1111/SecretMap.java
+++ src/main/java/com/likelion/java1111/SecretMap.java
@@ -1,5 +1,7 @@
 package com.likelion.java1111;
 
+import java.util.Arrays;
+
 //
 public class SecretMap {
     public void DecimalToBinary(int num){
@@ -11,6 +13,35 @@ public class SecretMap {
                 System.out.print("0");
             }
         }
+    }
+
+    String getBinaryString(int decNum) {
+        String answer = "";
+        while(decNum > 0) {
+            answer+= decNum % 2;
+            decNum/= 2;
+        }
+        return answer;
+    }
+
+
+    String solution(int n, int[] arr1, int[] arr2) {
+        String answer = "";
+        String[] sArr1 = new String[n];
+        String[] sArr2 = new String[n];
+        for (int i = 0; i < n; i++) {
+            // arr1
+            for (int j = 0; j < n; j++) {
+                String binStr1 = getBinaryString(arr1[i]);
+                String binStr2 = getBinaryString(arr2[i]);
+                sArr1[i] = "0".repeat(n - binStr1.length()) + binStr1;
+                sArr2[i] = "0".repeat(n - binStr2.length()) + getBinaryString(arr2[i]);
+            }
+        }
+        System.out.println(Arrays.toString(sArr1));
+        System.out.println(Arrays.toString(sArr2));
+
+        return answer;
     }
     public static void main(String[] args) {
 
