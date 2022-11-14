5591f4c79d46233e791be3449afe1f3b8ef5fb95
psi5059 <psi5059@naver.com>
2022-11-14 10:48:52 +0900

    에라토스테네스의 체 복습

--- src/main/java/com/likelion/java1114/SieveOfEratosthenes.java
+++ src/main/java/com/likelion/java1114/SieveOfEratosthenes.java
@@ -1,23 +1,38 @@
 package com.likelion.java1114;
 
 public class SieveOfEratosthenes {
-    public int solution(int n){
-        int answer = 0;
-        int[] arr = new int[n+1];
-
-        for (int i = 2; i<=n; i++){
-            arr[i]=i;
-        }
-
-        for (int i = 0; i*i < n; i++) {
-            if (arr[i]%2 == 0){
-                arr[i] = 0;
-            }
-            System.out.println(arr[i]);
-        }
-        return answer;
-    }
     public static void main(String[] args) {
+        int N = 50;
+        int[]arr = new int[N-1];
+        //System.out.println(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 2;
+
+        }
+
+        //2를 제외한 2의 배수 0으로 만들기
+        /*
+          arr[4] = 0;
+          arr[6] = 0;
+          arr[8] = 0;
+        */
+        for (int i = 0; i * i<=N; i++) {
+            System.out.println("i = "+ i);
+            if (arr[i] != 0){
+                for (int j = (i * 2 + 2); j < arr.length; j+=arr[i]) {
+                    System.out.println(j);
+                    arr[j]=0;
+                }
+            }
+        }
+
+        System.out.printf("%d %d\n", 2, arr[2]);
+        System.out.printf("%d %d\n", 4, arr[4]);
+        System.out.printf("%d %d\n", 5, arr[5]);
+        System.out.printf("%d %d\n", 7, arr[7]);
+        System.out.printf("%d %d\n", 47, arr[47]);
+        System.out.printf("%d %d\n", 48, arr[48]);
 
     }
 
