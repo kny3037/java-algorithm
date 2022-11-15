18ed340ec20dba3825633bb3d429891374bf2a1a
psi5059 <psi5059@naver.com>
2022-11-15 09:29:54 +0900

    QuickSort 진행 중

---
+++ src/main/java/com/likelion/java1115/QuickSort.java
@@ -0,0 +1,27 @@
+package com.likelion.java1115;
+
+public class QuickSort {
+    public void solution(int[] arr, int start, int end){
+        if (start >= end) return;
+        // start가 end보가 크거나 같으면 정렬값이 1개 이하니 정렬 안하고 return
+
+        int pivot = start;
+        //가장 왼쪽 값을 기준값으로 잡고
+        int left = start + 1;
+        int right = end;
+        //기준 값 +1이 왼쪽
+
+    }
+
+    public static void main(String[] args) {
+        var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
+
+        //1. 기준 값 뽑는 로직 구현
+
+
+        //2. 기준 값 기준으로 왼쪽 오른쪽으로 나누어 담는 로직 구현
+
+
+        //
+    }
+}
