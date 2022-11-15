3c7865635841e098c415309d1a9ca17a7313e735
psi5059 <psi5059@naver.com>
2022-11-15 10:16:21 +0900

    QuickSort 진행 중

--- src/main/java/com/likelion/java1115/QuickSort.java
+++ src/main/java/com/likelion/java1115/QuickSort.java
@@ -1,27 +1,34 @@
 package com.likelion.java1115;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuickSort {
-    public void solution(int[] arr, int start, int end){
-        if (start >= end) return;
-        // start가 end보가 크거나 같으면 정렬값이 1개 이하니 정렬 안하고 return
+    public List<Integer>sort(List<Integer> arr){
+        //1. 기준 값 뽑는 로직 구현
+        int pivot = arr.get(arr.size()/2);  // 5
+        System.out.println(pivot);
+        //2. 기준 값 기준으로 왼쪽 오른쪽으로 나누어 담는 로직 구현
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
 
-        int pivot = start;
-        //가장 왼쪽 값을 기준값으로 잡고
-        int left = start + 1;
-        int right = end;
-        //기준 값 +1이 왼쪽
+        for (int i = 0; i < arr.size(); i++) {
+            if (pivot > arr.get(i)) left.add(arr.get(i));
+            else if (pivot < arr.get(i)) right.add(arr.get(i));
+        }
+        System.out.println(left); //
+        System.out.println(right); // 20, 18, 19, 25, 40, 50
 
+        return sort(left) + pivot + sort(right);
     }
 
     public static void main(String[] args) {
-        var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
 
-        //1. 기준 값 뽑는 로직 구현
+        var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};  //size : 8
+        List<Integer> al = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            al.add(arr[i]);
+        }
 
-
-        //2. 기준 값 기준으로 왼쪽 오른쪽으로 나누어 담는 로직 구현
-
-
-        //
     }
 }
