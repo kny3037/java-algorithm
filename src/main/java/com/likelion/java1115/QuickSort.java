8d0407bf13217dc8fb1f1b5c820ff1a760f6fd48
psi5059 <psi5059@naver.com>
2022-11-15 10:36:48 +0900

    QuickSort List 합치기

--- src/main/java/com/likelion/java1115/QuickSort.java
+++ src/main/java/com/likelion/java1115/QuickSort.java
@@ -2,8 +2,18 @@ package com.likelion.java1115;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class QuickSort {
+
+    public List<Integer> merge(List<Integer> left, List<Integer> mid, List<Integer> right){
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(mid);
+        answer.addAll(right);
+        return answer;
+    }
+
     public List<Integer>sort(List<Integer> arr){
         //1. 기준 값 뽑는 로직 구현
         int pivot = arr.get(arr.size()/2);  // 5
@@ -11,15 +21,17 @@ public class QuickSort {
         //2. 기준 값 기준으로 왼쪽 오른쪽으로 나누어 담는 로직 구현
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
 
         for (int i = 0; i < arr.size(); i++) {
             if (pivot > arr.get(i)) left.add(arr.get(i));
             else if (pivot < arr.get(i)) right.add(arr.get(i));
+            else mid.add(arr.get(i));
         }
         System.out.println(left); //
         System.out.println(right); // 20, 18, 19, 25, 40, 50
-
-        return sort(left) + pivot + sort(right);
+        // list를 합치는 연산
+        return merge(sort(left), sort(mid), sort(right));
     }
 
     public static void main(String[] args) {
@@ -29,6 +41,9 @@ public class QuickSort {
         for (int i = 0; i < arr.length; i++) {
             al.add(arr[i]);
         }
+
+        QuickSort qs = new QuickSort();
+        qs.sort(arr);
 
     }
 }
