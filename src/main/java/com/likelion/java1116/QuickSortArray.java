0cbbae67324293fa0ebcad44ea1ad9e2ed35d264
psi5059 <psi5059@naver.com>
2022-11-17 09:46:38 +0900

    QuickSort 로직 구현

--- src/main/java/com/likelion/java1116/QuickSortArray.java
+++ src/main/java/com/likelion/java1116/QuickSortArray.java
@@ -1,18 +1,46 @@
 package com.likelion.java1116;
 
+import com.likelion.java1115.QuickSort;
+
+import java.util.Arrays;
+
 public class QuickSortArray {
-    public static void main(String[] args) {
-        var arr = new int[]{20,18,5,19,40,50,5,25};
+    public int[] sort(int []arr, int startIdx, int endIdx){
         int pivot = arr[arr.length / 2];
         int leftIdx = 0;
-        int rightIdx = 0;
+        int rightIdx = arr.length - 1;
 
-        if (leftIdx >= rightIdx) return;
+        //--------------여기 아랫줄이 반복됨.----------------
+        //언제까지 반복되는지? leftIdx == rightIdx일 때도 아래 로직이 반복됨
+        //ex) leftIdx = 3, rightIdx = 3일 때도 반복
 
-        while (arr[leftIdx] < pivot) leftIdx += 1;
-        //leftIdx가 4가 되어 있을 것
-        while (arr[rightIdx] > pivot) leftIdx -= 1;
-        //leftIdx = 4, right = 7 그대로
-        //왜? 25는 교환 대상이기 때문에
+        //왼쪽과 오른쪽 각각 교환함
+        //index를 정하는 부분 pivot까지 올 수도 있다.
+        while(leftIdx <= rightIdx) {
+            while (arr[leftIdx] < pivot) leftIdx += 1;
+            //leftIdx가 4가 되어 있을 것
+            while (arr[rightIdx] > pivot) rightIdx -= 1;
+            //leftIdx = 4, right = 7 그대로
+            //왜? 25는 교환 대상이기 때문에
+            if (leftIdx <= rightIdx) {
+                int temp = arr[leftIdx];
+                arr[leftIdx] = arr[rightIdx];
+                arr[rightIdx] = temp;
+                leftIdx += 1;
+                rightIdx -= 1;
+            }
+        }
+        System.out.printf("leftIdx : %d, rightIdx : %d", leftIdx, rightIdx);
+        System.out.println(Arrays.toString(arr));
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{20,18,5,19,40,50,5,25};
+        QuickSort qs = new QuickSort();
+
+
+
     }
 }
