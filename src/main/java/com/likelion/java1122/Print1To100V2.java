00ea34cf9fa3984e68acaf1beff8e2f173d9be36
psi5059 <psi5059@naver.com>
2022-11-22 09:16:40 +0900

    재귀 1부터 100 출력

--- src/main/java/com/likelion/java1122/Print1To100V2.java
+++ src/main/java/com/likelion/java1122/Print1To100V2.java
@@ -2,8 +2,9 @@ package com.likelion.java1122;
 
 public class Print1To100V2 {
     public static void printTo100(int n){
+        if (n > 100) return;
         System.out.println(n);
-        printTo100(n);
+        printTo100(n + 1);
     }
     public static void main(String[] args) {
         printTo100(1);
