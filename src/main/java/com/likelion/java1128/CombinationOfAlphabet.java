428c4828c44e348890509dd3312d6298fd5f2324
psi5059 <psi5059@naver.com>
2022-11-28 09:19:46 +0900

    알파벳 한 글자의 모든 경우의 수, 두 글자의 모든 경우의 수 출력

---
+++ src/main/java/com/likelion/java1128/CombinationOfAlphabet.java
@@ -0,0 +1,17 @@
+package com.likelion.java1128;
+
+public class CombinationOfAlphabet {
+    public static void main(String[] args) {
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+        for (char i = 'A'; i <= 'Z'; i++) {
+            for (char j = 'A'; j <= 'Z'; j++) {
+                System.out.println(i + "" + j);
+            }
+        }
+    }
+}
