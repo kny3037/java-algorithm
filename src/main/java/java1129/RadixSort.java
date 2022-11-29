6d4a008bc73e8fb6b14daf9f343f7edebad5baee
psi5059 <psi5059@naver.com>
2022-11-29 09:42:28 +0900

    Radix Sort 한 자릿 수 숫자 정렬

---
+++ src/main/java/java1129/RadixSort.java
@@ -0,0 +1,28 @@
+package java1129;
+
+import java.util.Arrays;
+
+public class RadixSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{7,4,5,9,1,0};
+        int[] radix = new int[10];
+        Arrays.fill(radix,-1);
+
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+            radix[arr[i]] = arr[i];
+        }
+
+        System.out.println(Arrays.toString(radix));
+
+        int cnt = 0;
+        for (int i = 0; i < radix.length; i++) {
+            if (radix[i] != -1){
+                arr[cnt++] = radix[i];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
