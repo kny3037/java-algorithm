b43a7f9a7e8cbcea298ae2a85935143f201265c6
nayeong kim <psi5059@naver.com>
2022-11-30 22:32:55 +0900

    프로그래머스 전화번호 목록 풀이

--- src/main/java/com/likelion/java1028/PhoneNumListArray.java
+++ src/main/java/com/likelion/java1028/PhoneNumListArray.java
@@ -3,6 +3,8 @@ package com.likelion.java1028;
 import java.util.HashSet;
 // 아무 한 전화번호가 다른 전화번호의 시작부터의 일부와 동일하면 false
 // 서로 연관이 없으면 true return
+// 전화번호부를 돌면서 전화번호부를 본인의 전화번호부 길이까지 자르고,
+// set에 저장된 번호와 자른 전화번호부가 동일한지 확인.
 
 public class PhoneNumListArray {
     public boolean solution(String[] phone_book) {
