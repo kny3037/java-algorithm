04f4af597f7e4f74fbe1ea8708db5f90f8d1b2b4
nayeong kim <psi5059@naver.com>
2022-12-05 09:27:34 +0900

    Heap Sort 정렬 [6,5,7,8] -> [8,6,7,5]

---
+++ src/main/java/com/likelion/java1205/HeapSort.java
@@ -0,0 +1,20 @@
+package com.likelion.java1205;
+
+import java.util.Arrays;
+
+public class HeapSort {
+    public static void main(String[] args) {
+        int[]arr = {6,5,7,8};
+
+        int temp = arr[1];
+        arr[1] = arr[3];
+        arr[3] = temp;
+        System.out.println(Arrays.toString(arr));
+
+        temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
