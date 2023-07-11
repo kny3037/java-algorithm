7ce79291256bf7a3ae70cda029a1147090fcadb1
nayeong kim <psi505968@gmail.com>
2023-07-11 15:27:41 +0900

    백준 단계별 풀어보기 3단계

---
+++ src/main/java/com/likelion/baekjoon/level3/B10952.java
@@ -0,0 +1,15 @@
+package com.likelion.baekjoon.level3;
+
+import java.util.Scanner;
+
+public class B10952 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+
+    }
+}
